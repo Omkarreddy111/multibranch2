bda82a21ab2fe3b2882d432410547508ff2a3f52
Omkarreddy111 <99641843+Omkarreddy111@users.noreply.github.com>
2023-05-23 21:21:51 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 TASK=`sudo aws ecs list-tasks --cluster PRODUCTION --service-name Demo_app --desired-status RUNNING --region us-east-2 --output text | awk '{print $2}'`
-sudo aws ecs stop-task --cluster PRODUCTION --task $TASK --region us-east-2
+if [[ -z $TASK ]]
+then
+echo "No tasks are running"
+else
+sudo aws ecs stop-task --region us-east-2 --cluster PRODUCTION --task $TASK
 echo "Task stopped successfully"
+fi
