62283bb40513b4d47f948e37f5dc71b4d001df76
Omkarreddy111 <99641843+Omkarreddy111@users.noreply.github.com>
2023-05-23 22:52:18 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-TASK=`sudo aws ecs list-tasks --cluster QAt2 --service-name Demo_app --desired-status RUNNING --region us-east-2 --output text | awk '{print $2}'`
+TASK=`sudo aws ecs list-tasks --cluster QAt2 --service-name Demo_app --desired-status RUNNING --region us-east-2 --output text --query taskArns`
 if [[ -z $TASK ]]
 then
 echo "No tasks are running"
 
 else
-sudo aws ecs stop-task --region us-east-2 --cluster QAt2 --task $TASK
+for task in `echo $TASK`
+do
+sudo aws ecs stop-task --region us-east-2 --cluster QAt2 --task $task
 
 echo "Task stopped and updated sucessfully successfully"
+done
 fi
