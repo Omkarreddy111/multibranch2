a0a51e038fc5284ec2726cb8cb49b06a3239ccac
Omkarreddy111 <99641843+Omkarreddy111@users.noreply.github.com>
2023-05-23 20:25:00 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-TASK=`sudo aws ecs list-tasks --cluster DEMO --service-name Demo_app --desired-status RUNNING --region ap-south-1 --output text | awk '{print $2}'`
-sudo aws ecs stop-task --cluster DEMO --task $TASK
+TASK=`sudo aws ecs list-tasks --cluster PRODUCTION --service-name Demo_app --desired-status RUNNING --region ap-south-1 --output text | awk '{print $2}'`
+sudo aws ecs stop-task --cluster PRODUCTION --task $TASK
 echo "Task stopped successfully"
